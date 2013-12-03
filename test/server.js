632d7ef9bcd9d6fe06b89353fad3a502f2701a84
Clint Walker <clint@neonrelic.com>
2013-12-03 11:08:43 +0800

    Update API server stub to support CORS

--- test/server.js
+++ test/server.js
@@ -7,8 +7,38 @@ var express = require('express');
 
 var app = express();
 
+var allowCrossDomain = function(req, res, next) {
+
+  var headers = [
+      'Content-Type'
+    // Required for CORS preflight
+    , 'X-Requested-With'
+  ]
+
+  var methods = [
+      'GET'
+    , 'POST'
+    , 'PUT'
+    , 'DELETE'
+    // Options is required for CORS preflight
+    // https://developer.mozilla.org/en-US/docs/HTTP/Access_control_CORS
+    , 'OPTIONS'
+  ]
+
+  res.header( 'Access-Control-Allow-Origin', '*' )
+  res.header( 'Access-Control-Allow-Credentials', 'true' )
+  res.header( 'Access-Control-Allow-Methods', methods.join(',') )
+  res.header( 'Access-Control-Allow-Headers', headers.join(',') )
+
+  if (req.method === 'OPTIONS') res.json( 200 )
+  else next()
+
+}
+
+app.use( allowCrossDomain );
 
 app.all('*', function (req, res) {
+  console.log('Request:', new Date() );
   res.send('-');
 });
 
